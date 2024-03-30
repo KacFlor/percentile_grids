6f3179ff6eddfa404c8bffca5af9348bf80ecefe
KacFlor <kacper.florczyk@onet.pl>
2024-03-30 21:56:04 +0100

    new file Frame.py + photo show inside main

--- main.py
+++ main.py
@@ -5,76 +5,9 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
 from Menu import Menu
+from Frame import Frame
 
 import sys
-
-wiek = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
-waga = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
-
-centyl_3_wiek = [3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17, 17.5, 18]
-centyl_3_waga = [12, 13, 14, 14.5, 15, 16, 17, 18, 18.5, 19, 20.5, 21.5, 22.5, 23.5, 24.5, 26, 27, 28, 30, 31.5, 33.5, 35, 38, 40, 43, 45, 47, 49.5, 51, 52.5, 54]
-
-centyl_10_wiek = [3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17, 17.5, 18]
-centyl_10_waga = [13, 13.75, 14.75, 15.5, 16, 17, 18, 19, 20.25, 21.25, 22.5, 23.5, 24.75, 25.75, 27, 28, 29.75, 31.25, 33, 35, 37, 39.75, 42, 44.75, 47, 49.25, 51.5, 53.5, 55.25, 57, 58.25]
-
-centyl_25_wiek = [3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17, 17.5, 18]
-centyl_25_waga = [13.75, 14.75, 15.75, 16.5, 17.25, 18.5, 19.5, 20.5, 22, 23, 24.75, 26, 27, 28.5, 30, 31.5, 33.1, 35, 37, 39.25, 41.5, 44.25, 46.75, 49.9, 52, 54.5, 56.5, 58.5, 60, 61.75, 63.5]
-
-centyl_50_wiek = [3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17, 17.5, 18]
-centyl_50_waga = [15, 16, 17, 18, 19, 20.25, 21.5, 22.9, 24.5, 26, 27.5, 29, 30.75, 32.5, 34.25, 36, 38, 40.25, 43, 45.25, 48, 51, 53.75, 56.25, 59, 61, 63, 65, 67, 68.5, 70]
-
-centyl_75_wiek = [3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17, 17.5, 18]
-centyl_75_waga = [16.5, 17.5, 18.75, 20, 21.25, 22.5, 24.5, 26, 27.75, 29.9, 31.5, 33.25, 35.5, 37.25, 39.9, 41.9, 44.75, 47, 50, 53, 55.75, 59, 62, 64.75, 67, 69.25, 71.25, 73.1, 74.9, 76.5, 78]
-
-centyl_90_wiek = [3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17, 17.5, 18]
-centyl_90_waga = [18, 19.25, 20.75, 22, 23.5, 25.5, 27.5, 29.5, 32, 34.25, 36.75, 38.75, 41.25, 44, 46.75, 49.75, 52.5, 55.1, 58.6, 61.9, 65, 68.5, 71, 73.75, 76, 78.25, 80, 82, 83.25, 84.9, 86]
-
-centyl_97_wiek = [3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17, 17.5, 18]
-centyl_97_waga = [20, 21.5, 23, 24.9, 26.75, 29.75, 32.1, 34.9, 37.5, 40.5, 43.5, 46.25, 49.5, 52.5, 56, 59, 62.75, 66, 69.75, 73.1, 76.5, 80, 82.75, 85, 87, 89, 90.5, 92.1, 93.5, 94.75, 96]
-
-
-
-def frame(x,y,z,a):
-    plt.figure(figsize=(6, 9))
-    plt.plot(centyl_3_wiek, centyl_3_waga, label='Centyl 3', color='black')
-    plt.plot(centyl_10_wiek, centyl_10_waga, label='Centyl 10', color='black',  linestyle='--')
-    plt.plot(centyl_25_wiek, centyl_25_waga, label='Centyl 25', color='black',  linestyle=':')
-    plt.plot(centyl_50_wiek, centyl_50_waga, label='Centyl 50', color='black',  linestyle='solid')
-    plt.plot(centyl_75_wiek, centyl_75_waga, label='Centyl 75', color='black',  linestyle=':')
-    plt.plot(centyl_90_wiek, centyl_90_waga, label='Centyl 90', color='black',  linestyle='--')
-    plt.plot(centyl_97_wiek, centyl_97_waga, label='Centyl 97', color='black',  linestyle='solid')
-    if(z == 1):
-        plt.scatter(x, y, label='Punkty XY', color='red', s=5)
-    if(z == 2):
-        plt.plot(y, x, label='krzywa', color='red', linestyle=':')
-    if(z == 3):
-        for i in range(a):
-            plt.scatter(x[i], y[i], label='Punkty XY', color='red', s=5)
-
-    plt.text(centyl_3_wiek[-1], centyl_3_waga[-1], ' c3', verticalalignment='bottom')
-    plt.text(centyl_10_wiek[-1], centyl_10_waga[-1], ' c10', verticalalignment='bottom')
-    plt.text(centyl_25_wiek[-1], centyl_25_waga[-1], ' c25', verticalalignment='bottom')
-    plt.text(centyl_50_wiek[-1], centyl_50_waga[-1], ' c50', verticalalignment='bottom')
-    plt.text(centyl_75_wiek[-1], centyl_75_waga[-1], ' c75', verticalalignment='bottom')
-    plt.text(centyl_90_wiek[-1], centyl_90_waga[-1], ' c90', verticalalignment='bottom')
-    plt.text(centyl_97_wiek[-1], centyl_97_waga[-1], ' c97', verticalalignment='bottom')
-
-    plt.title('Siatka centylowa')
-    plt.xlabel('Wiek (lata)')
-    plt.ylabel('Waga (kg)')
-
-    plt.xticks(wiek)
-    plt.xlim(3, 18)
-    plt.ylim(10, 100)
-    plt.yticks(waga)
-
-    plt.grid(True)
-
-    fig = plt.gcf()
-    fig.tight_layout()
-    fig.savefig('percentile_grid.png')
-    plt.show(block=False)
-    plt.show()
 
 class SecondWindow_for_single(QDialog):
     def __init__(self, parent=None):
@@ -145,7 +78,7 @@ class SecondWindow_for_multiple(QDialog):
             for i in range(self.a):
                 self.open_second_window_for_single()
             else:
-                frame(self.x, self.y, 3, self.a)
+                frame.frame_init(self.x, self.y, 3, self.a)
 
             if not (100 > self.a > 0 ):
                 raise ValueError("Wartości poza zakresem")
@@ -207,28 +140,37 @@ class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setWindowTitle("KidNet")
-        self.setGeometry(200,200,300,300)
+        self.setGeometry(200,40,300,300)
+        self.setFixedSize(1000,900)
 
         central_widget = QWidget()
-        layout = QVBoxLayout()
-        central_widget.setFixedSize(QSize(300, 250))
-        central_widget.setLayout(layout)
+        main_layout = QHBoxLayout()
+        central_widget.setLayout(main_layout)
         self.setCentralWidget(central_widget)
+
+        left_layout = QVBoxLayout()
+        main_layout.addLayout(left_layout)
+
+        right_layout = QVBoxLayout()
+        main_layout.addLayout(right_layout)
 
         self.button_s = QPushButton("Jedne dane")
         self.button_s.setCheckable(True)
         self.button_s.setFixedSize(QSize(250,30))
-        layout.addWidget(self.button_s)
+        left_layout.addWidget(self.button_s)
 
         self.button_m = QPushButton("Wiele danych")
         self.button_m.setCheckable(True)
         self.button_m.setFixedSize(QSize(250, 30))
-        layout.addWidget(self.button_m)
+        left_layout.addWidget(self.button_m)
 
         self.button_c = QPushButton("Krzywa danych")
         self.button_c.setCheckable(True)
         self.button_c.setFixedSize(QSize(250, 30))
-        layout.addWidget(self.button_c)
+        left_layout.addWidget(self.button_c)
+
+        self.label_image = QLabel()
+        right_layout.addWidget(self.label_image)
 
         self.second_window = None
         self.button_s.clicked.connect(self.open_second_window_for_single)
@@ -242,11 +184,13 @@ class MainWindow(QMainWindow):
         if self.second_window.result() == QDialog.Accepted:
             x = self.second_window.x
             y = self.second_window.y
-            frame(x, y, 1, 0)
+            frame.frame_init(x, y, 1, 0)
+            self.label_image.setPixmap(QPixmap('percentile_grid.png'))
 
     def open_second_window_for_multiple(self):
         self.second_window = SecondWindow_for_multiple(self)
         self.second_window.exec_()
+        self.label_image.setPixmap(QPixmap('percentile_grid.png'))
 
     def open_second_window_for_curve(self):
         age = 3
@@ -255,20 +199,23 @@ class MainWindow(QMainWindow):
                   14.5, 15, 15.5, 16, 16.5, 17, 17.5, 18]
 
         while age <= 18:
+            if self.second_window is not None:
+                self.second_window.deleteLater()
             self.second_window = SecondWindow_for_curve(age, self)
             self.second_window.exec_()
-
             if self.second_window.result() == QDialog.Accepted:
                 self.x.append(self.second_window.x)
             age += 0.5
 
-        if self.x:  # Sprawdzenie, czy lista self.x nie jest pusta
-            frame(self.x, self.y, 2, 0)
+        if self.x:
+            frame.frame_init(self.x, self.y, 2, 0)
+            self.label_image.setPixmap(QPixmap('percentile_grid.png'))
 
 
 if __name__ == "__main__":
     app = QApplication([])
     window = MainWindow()
+    frame = Frame()
     window.show()
     app.exec_()
 
